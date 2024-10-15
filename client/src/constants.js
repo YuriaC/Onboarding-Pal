0bdf5b91676dc3aafa77562a198ca0675a8cf915
Albert Dong <albertddong@hotmail.com>
2024-10-15 12:51:11 -0500

    Added more endpoints to constants file

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,5 +2,6 @@
 export const API_BASE_URL = 'http://localhost:5173/api'
 export const USER_ENDPOINT = `${API_BASE_URL}/users`
 export const HOUSE_ENDPOINT = `${API_BASE_URL}/houses`
-export const EMPLOYEE_ENDPOINT = `${API_BASE_URL}/employees`
 export const CONTACT_ENDPOINT = `${API_BASE_URL}/contacts`
+export const REPORT_ENDPOINT = `${API_BASE_URL}/reports`
+export const COMMENT_ENDPOINT = `${API_BASE_URL}/comments`
