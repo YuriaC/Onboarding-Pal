74677b0505b01c11912d8f65bb716c52fc74b79c
Minh Meo <dminhnguyen161@gmail.com>
2024-10-17 16:21:12 -0700

    fixed register and JWT to cookies

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -7,14 +7,13 @@ const validator = require('validator')
 const authenticateJWT = (req, res, next) => {
 
   try {
-    // get token from header
-    const token = req.headers.authorization.split(' ')[1];
+    // get token from cookie
+    const token = req.cookies['auth_token'];
     if (!token || validator.isEmpty(token)) {
       return res.status(401).json('No token provided');
     }
 
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
     req.body.userId = decoded.id;
     req.body.username = decoded.username;
     req.body.role = decoded.role
