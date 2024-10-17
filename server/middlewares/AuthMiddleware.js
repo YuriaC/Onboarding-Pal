7ed9087c2cd7a8fa8ad6f62df79f18ea54cf04af
Albert Dong <albertddong@hotmail.com>
2024-10-17 15:52:39 -0500

    Got authenticateJWT and isHR to work with each other

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -3,9 +3,6 @@
 const jwt = require('jsonwebtoken');
 const validator = require('validator')
 
-// Replace with your own secret in production and store it securely
-// const JWT_SECRET = process.env.JWT_SECRET || "secret";
-
 // Middleware to authenticate JWT
 const authenticateJWT = (req, res, next) => {
 
@@ -30,26 +27,8 @@ const authenticateJWT = (req, res, next) => {
 
 const isHR = (req, res, next) => {
   
-  // decode token
-  try {
-    // get token from header
-    // const token = req.headers.authorization.split(' ')[1];
-    // if (!token || validator.isEmpty(token)) {
-    //   return res.status(401).json('No token provided');
-    // }
-
-    // const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    if (req.body.role !== 'hr') {
-      return res.status(403).json('Must be HR to access this!')
-    }
-
-    // assign data inside the token to the request body so that we can directly access these data in the request object in the route handler functions
-    // req.body.userId = decoded.id;
-    // req.body.username = decoded.username;
-    // req.body.role = decoded.role
-  } catch (err) {
-    return res.status(401).json('Invalid token');
+  if (req.body.role !== 'hr') {
+    return res.status(403).json('Must be HR to access this!')
   }
 
   next();
--- server/routers/HouseRouter.js
+++ server/routers/HouseRouter.js
@@ -2,15 +2,10 @@ const houseRouter = require("express").Router();
 const houseController = require('../controllers/HouseController');
 const { authenticateJWT, isHR } = require("../middlewares/AuthMiddleware");
 
-
-//  Sample router
-
-// NOTE : ADD MIDDLEWARES
 houseRouter
     .get('/', authenticateJWT, isHR, houseController.getHouses)
     .get('/details/:houseId', houseController.getHouseById)
     .post('/', authenticateJWT, isHR, houseController.addHouse)
     .delete('/delete/:houseId', authenticateJWT, isHR, houseController.deleteHouse)
 
-// code gen
 module.exports = houseRouter;
