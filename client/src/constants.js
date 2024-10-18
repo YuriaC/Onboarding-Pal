4df6539adbc2837ce72a30117513c55772b48118
Albert Dong <albertddong@hotmail.com>
2024-10-18 18:11:29 -0500

    Trying out Material UI Accordion in HousingMgmt

--- client/src/constants.js
+++ client/src/constants.js
@@ -7,8 +7,8 @@ export const REPORT_ENDPOINT = `${API_BASE_URL}/reports`
 export const COMMENT_ENDPOINT = `${API_BASE_URL}/comments`
 
 // HR
-// export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzLCJ1c2VybmFtZSI6ImFiYyIsInJvbGUiOiJociIsImlhdCI6MTcyOTIxNzUxNSwiZXhwIjoxNzI5MjI4MzE1fQ.XtfQU0ap2OVB7e-pE4Igasl-NFiETZTk8UNVGFmFeA4'
+export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzLCJ1c2VybmFtZSI6IkhSVGVzdCIsInJvbGUiOiJociIsImlhdCI6MTcyOTI5MjY0NiwiZXhwIjoxNzI5MzAzNDQ2fQ.eCjyM75pBy62dPuvxbKdoHGmVtcSy-qe-EBHEEe4h90'
 // Employee
-export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzLCJ1c2VybmFtZSI6IkVtcGxveWVlVGVzdCIsInJvbGUiOiJlbXBsb3llZSIsImlhdCI6MTcyOTI4NTQ1OCwiZXhwIjoxNzI5Mjk2MjU4fQ.nk-ooH6EnDaJFmc_BEDq36jAVKcRxaegUH1axl-v6Po'
+// export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzLCJ1c2VybmFtZSI6IkVtcGxveWVlVGVzdCIsInJvbGUiOiJlbXBsb3llZSIsImlhdCI6MTcyOTI4NTQ1OCwiZXhwIjoxNzI5Mjk2MjU4fQ.nk-ooH6EnDaJFmc_BEDq36jAVKcRxaegUH1axl-v6Po'
 
 export const username = 'EmployeeTest'
