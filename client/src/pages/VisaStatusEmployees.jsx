ecb0692fc608ef61940cce262ace1ec49a4c51ab
YC <sapphirechengyj@gmail.com>
2024-10-20 17:41:08 -0400

    initial commit

--- client/src/pages/VisaStatusEmployees.jsx
+++ client/src/pages/VisaStatusEmployees.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { token, USER_ENDPOINT, username } from '../constants'
+import axios from 'axios'
 
 const VisaStatusEmployees = () => {
-
+    // only apply to employee OPT visa status
+    // get authentication information
     
 
     return (
